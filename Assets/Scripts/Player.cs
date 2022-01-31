cb507249b0166fd43156389073502e34c901acdd
fernaosilveira <fernao.s.silveira@gmail.com>
2022-01-31 16:02:01 -0300

    Add jump and movement friction to prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float jumpForce = 1f;
+    public Vector2 dash = new Vector2(5, 0);
 
     void Update()
+    {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -17,6 +26,24 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * jumpForce; 
         }
     }
 }
